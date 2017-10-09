d6b2cbd0de839033e5284673eb4af26432f33aee
chacal89 <32643069+chacal89@users.noreply.github.com>
2017-10-09 16:26:20 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@ echo limpando possiveis instalações
 rm -rf /usr/src/ast*
 rm -rf /usr/src/dongle*
 rm -rf /usr/src/chan*
-read year 
+read year
 
 clear
 echo Abaixo comente a linha referente ao CD
-read year 
+read year
 nano /etc/apt/sources.list
 echo Atualizando repositorios
 read year
@@ -15,13 +15,15 @@ apt-get update
 apt-get upgrade
 echo Instalando dependencias
 read year
-apt-get install openssh-server php-pear apache2-mpm-prefork php-http php5-gd php5-cli php5-mysql mysql-server sudo libapache2-mod-php5 -y
-apt-get install unixodbc-dev libmyodbc sudo ntp build-essential libncurses5-dev libssl-dev libxml2-dev libsqlite3-dev uuid-dev vim-nox linux-headers-`uname -r` subversion unzip automake lshw hdparm -y
-apt-get install -y mysql-server build-essential linux-headers-`uname -r` subversion checkinstall bison flex apache2 php5 php5-curl php5-cli php5-mysql php-pear php-db php5-gd curl sox libncurses5-dev libssl-dev libmysqlclient15-dev mpg123 libxml2-dev -y
+apt-get install openssh-server php-pear apache2-mpm-prefork php-http php5-gd php                                                                                        5-cli php5-mysql mysql-server sudo libapache2-mod-php5 -y
+apt-get install unixodbc-dev libmyodbc sudo ntp build-essential libncurses5-dev                                                                                         libssl-dev libxml2-dev libsqlite3-dev uuid-dev vim-nox linux-headers-`uname -r`                                                                                         subversion unzip automake lshw hdparm -y
+apt-get install -y mysql-server build-essential linux-headers-`uname -r` subvers                                                                                        ion checkinstall bison flex apache2 php5 php5-curl php5-cli php5-mysql php-pear                                                                                         php-db php5-gd curl sox libncurses5-dev libssl-dev libmysqlclient15-dev mpg123 l                                                                                        ibxml2-dev -y
 apt-get install automake autoconf subversion p7zip p7zip-full -y
 apt-get install usb-modeswitch* -y
 apt-get install minicom -y
 dpkg-reconfigure tzdata
+apt-get install wget -y
+apt-get install mlocate -y
 apt-get install ntpdate -y
 ntpdate us.pool.ntp.org
 hwclock --systohc
@@ -30,7 +32,7 @@ apt-get install ntp -y
 echo Baixando Asterisk
 read year
 cd /usr/src
-wget http://smartadmin.pbsys.com.br/ast.tar.gz
+wget https://drive.google.com/uc?authuser=0&id=0B0Nk9bCPO4KRMzJEcWJNODJXbTg&expo                                                                                        rt=download
 tar xzvf ast.tar.gz
 cd /usr/src/asterisk-1.8.32.3/
 
@@ -38,7 +40,7 @@ echo Compilando asterisk
 read year
 ./configure --disable-asteriskssl
 contrib/scripts/get_mp3_source.sh
-echo Em extras sounds adicione mais sons e em addons adicionar o suporte a cdr_mysql
+echo Em extras sounds adicione mais sons e em addons adicionar o suporte a cdr_m                                                                                        ysql
 read year
 make menuselect
 echo Compilando Asterisk
@@ -47,7 +49,7 @@ make && make install && make config
 ########### ATÉ AQUI OK
 echo Criando Usuario asterisk
 read year
-adduser --system --group --home /var/lib/asterisk --no-create-home --gecos "Asterisk PBX" asterisk
+adduser --system --group --home /var/lib/asterisk --no-create-home --gecos "Aste                                                                                        risk PBX" asterisk
 
 echo Ajustando Permissões
 read year
@@ -73,11 +75,11 @@ read year
 cd /usr/src
 svn checkout https://github.com/bg111/asterisk-chan-dongle.git dongle-read-only
 cd /usr/src/dongle-read-only/trunk
-wget http://smartadmin.pbsys.com.br/config.guess --no-check-certificate
-wget http://smartadmin.pbsys.com.br/config.sub --no-check-certificate
+wget https://drive.google.com/uc?authuser=0&id=0B0Nk9bCPO4KRTURMaGlLUHBTMU0&expo                                                                                        rt=download --no-check-certificate
+wget https://drive.google.com/uc?authuser=0&id=0B0Nk9bCPO4KRYU9XeUdranU3Wmc&expo                                                                                        rt=download --no-check-certificate
 
 echo ADICIONAR TRECHO DE CODIGO AO PDISCOVERY
-echo "{ 0x12d1, 0x1506, { 3, 2, /* 0 */ } },		/* E303 Brasil by Giovanni Bosa */"
+echo "{ 0x12d1, 0x1506, { 3, 2, /* 0 */ } },            /* E303 Brasil by Giovan                                                                                        ni Bosa */"
 echo E COMENTAR LINHA ACIMA
 read year
 nano pdiscovery.c
@@ -102,8 +104,8 @@ echo "0 */1 * * * sudo sh /etc/asterisk/reiniciarasterisk.sh >> /dev/null"
 echo "  */5 * * * * sudo /etc/asterisk/verificamodem.php"
 echo "###  0 4 * * * sudo sh /etc/asterisk/reiniciageral.sh"
 echo " 0 */1 * * *  sudo /etc/asterisk/limpamodem.php >> /dev/null"
-echo 
-echo 
+echo
+echo
 echo "*/1 * * * *  sudo /etc/asterisk/monitorasterisk.php"
 echo "*/1 * * * *  sleep 5 && sudo /etc/asterisk/monitorasterisk.php"
 echo "*/1 * * * *  sleep 10 && sudo /etc/asterisk/monitorasterisk.php"
@@ -121,10 +123,10 @@ crontab -e
 
 
 echo Adicionar:
-echo 
-echo 'KERNEL=="ttyUSB*", SYMLINK+="hw-modem-0", MODE="0666", OWNER="asterisk", GROUP="uucp"'
-echo 'KERNEL=="ttyUSB*", SYMLINK+="hw-audio-0", MODE="0666", OWNER="asterisk", GROUP="uucp"'
-echo 'KERNEL=="ttyUSB*", SYMLINK+="hw-data-0", MODE="0666", OWNER="asterisk", GROUP="uucp"'
+echo
+echo 'KERNEL=="ttyUSB*", SYMLINK+="hw-modem-0", MODE="0666", OWNER="asterisk", G                                                                                        ROUP="uucp"'
+echo 'KERNEL=="ttyUSB*", SYMLINK+="hw-audio-0", MODE="0666", OWNER="asterisk", G                                                                                        ROUP="uucp"'
+echo 'KERNEL=="ttyUSB*", SYMLINK+="hw-data-0", MODE="0666", OWNER="asterisk", GR                                                                                        OUP="uucp"'
 read year
 nano /etc/udev/rules.d/50-udev.rules
 
@@ -133,13 +135,13 @@ mkdir repo
 cd repo
 echo Baixando Scripts AGI
 read year
-wget http://smartadmin.pbsys.com.br/agi-bin.tar.gz
+wget https://drive.google.com/uc?authuser=0&id=0B0Nk9bCPO4KRQkRRSUNPUmVTd1k&expo                                                                                        rt=download
 echo Baixando Arquivos de Configuração
 read year
-wget http://smartadmin.pbsys.com.br/asterisk.tar.gz
+wget https://drive.google.com/uc?authuser=0&id=0B0Nk9bCPO4KRSkd4NENKYzRZM2c&expo                                                                                        rt=download
 echo Baixando Modulo de envio SMS pela URL
 read year
-wget http://smartadmin.pbsys.com.br/enviador.tar.gz
+wget https://drive.google.com/uc?authuser=0&id=0B0Nk9bCPO4KROWRJTF81U09scGs&expo                                                                                        rt=download
 
 rm -rf /etc/asterisk
 tar -xzvf asterisk.tar.gz
@@ -184,4 +186,3 @@ chmod 777 -R /var/log/asterisk
 echo CTRL+C para cancelar reboot
 read year
 reboot
-
